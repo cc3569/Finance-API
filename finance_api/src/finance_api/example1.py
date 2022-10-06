7589444dc6a6413fb31d3326b3b629c1f15967b3
Cole Catron <unnecessary4024@gmail.com>
2022-10-06 15:43:54 -0700

    Delete example1.py

--- finance_api/src/finance_api/example1.py
+++
@@ -1,8 +0,0 @@
-from dupont_fw import *
-
-# calculates ROE using the DuPont Analysis Framework
-
-d = dupont() # "d" is an object of the "dupont" Class
-print(d.fw_calc(0.15, 10, 4)) # print() statement using the fw_calc() method
-
-# output an ROE value of 6.0
